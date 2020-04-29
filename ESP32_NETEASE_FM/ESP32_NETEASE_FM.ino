99f895ca8bb2891cf69b41d7816aaf0158aa7553
lxrmido <lxrmido@lxrmido.com>
2020-04-30 00:50:20 +0800

    ..

--- ESP32_NETEASE_FM/ESP32_NETEASE_FM.ino
+++ ESP32_NETEASE_FM/ESP32_NETEASE_FM.ino
@@ -17,6 +17,7 @@ const char* login_password =  LOGIN_PASSWORD;
 
 const int nextButton = 13;
 const int pauseButton = 35;
+const int likeButton = 34;
 
 bool playing = false;
 
@@ -24,6 +25,11 @@ char token[256];
 int musicId;
 char musicUrl[1024];
 char requestUrl[256];
+
+char cookie[1024];
+
+const char * headerkeys[] = {"set-cookie"};
+size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
 
 AudioGeneratorMP3 *mp3;
 AudioFileSourceHTTPStream *file;
@@ -52,6 +58,16 @@ void IRAM_ATTR pauseButtonInterrupt() {
   }
   last_interrupt_time = interrupt_time;
 }
+void IRAM_ATTR likeButtonInterrupt() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 200) 
+  {
+    Serial.println("Like Clicked");
+    buttonClicked = 3;
+  }
+  last_interrupt_time = interrupt_time;
+}
 
 const int preallocateBufferSize = 16*1024;
 const int preallocateCodecSize = 85332;
@@ -63,11 +79,13 @@ void setup() {
   delay(500);
   preallocateBuffer = malloc(preallocateBufferSize);
   preallocateCodec = malloc(preallocateCodecSize);
-  out = new AudioOutputI2S();
+  out = new AudioOutputI2S(0, 1);
   pinMode(nextButton, INPUT_PULLUP);
   pinMode(pauseButton, INPUT_PULLUP);
+  pinMode(likeButton, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(nextButton), nextButtonInterrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(pauseButton), pauseButtonInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(likeButton), likeButtonInterrupt, FALLING);
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -90,10 +108,36 @@ void setup() {
   login() && getMusic() && getMusicUrl() && playMusic();
 }
 
+void like(){
+  HTTPClient http;
+  out->SetGain(0.01);
+  sprintf(requestUrl, "%s/like?id=%d&t=%d", url_prefix, musicId, random(1, 100000));
+  http.collectHeaders(headerkeys, headerkeyssize);
+  http.addHeader("Cookie", cookie);
+  http.begin(requestUrl);
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.GET();
+  String payload = http.getString();
+  Serial.print("HTTP Status:");
+  Serial.println(httpCode);
+  Serial.print("HTTP payload:");
+  Serial.println(payload);
+  http.end();
+  if(httpCode == 200){
+    strcpy(cookie, http.header("set-cookie").c_str());
+    Serial.println(cookie);
+    Serial.println("liked");
+  }else{
+    Serial.println("like failed");
+  }
+  out->SetGain(1.0);
+}
+
 bool login(){
   while (true){
     HTTPClient http;
     sprintf(requestUrl, "%s/login/cellphone?phone=%s&password=%s", url_prefix, login_phone, login_password);
+    http.collectHeaders(headerkeys, headerkeyssize);
     http.begin(requestUrl);
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();
@@ -104,6 +148,8 @@ bool login(){
     Serial.println(payload);
     http.end();
     if(httpCode == 200){
+      strcpy(cookie, http.header("set-cookie").c_str());
+      Serial.println(cookie);
       Serial.println("logined success");
       const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
       DynamicJsonBuffer jsonBuffer(bufferSize);
@@ -128,6 +174,8 @@ bool getMusic(){
   while (true){
     HTTPClient http;
     sprintf(requestUrl, "%s/personal_fm?t=%d", url_prefix, random(1, 100000));
+    http.addHeader("Cookie", cookie);
+    http.collectHeaders(headerkeys, headerkeyssize);
     http.begin(requestUrl);
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();
@@ -138,6 +186,8 @@ bool getMusic(){
     Serial.println(payload);
     http.end();
     if(httpCode == 200){
+      strcpy(cookie, http.header("set-cookie").c_str());
+      Serial.println(cookie);
       Serial.println("Get music success");
       const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
       DynamicJsonBuffer jsonBuffer(bufferSize);
@@ -165,6 +215,8 @@ bool getMusicUrl(){
   while (true){
     HTTPClient http;
     sprintf(requestUrl, "%s/song/url?br=320000&id=%d", url_prefix, musicId);
+    http.addHeader("Cookie", cookie);
+    http.collectHeaders(headerkeys, headerkeyssize);
     http.begin(requestUrl);
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();
@@ -175,6 +227,8 @@ bool getMusicUrl(){
     Serial.println(payload);
     http.end();
     if(httpCode == 200){
+      strcpy(cookie, http.header("set-cookie").c_str());
+      Serial.println(cookie);
       Serial.println("Get url success");
       const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
       DynamicJsonBuffer jsonBuffer(bufferSize);
@@ -197,24 +251,6 @@ bool getMusicUrl(){
 }
 
 bool playMusic(){
-  if (mp3) {
-    //Serial.println("Stop decode.");
-    //mp3->stop();
-    //delete mp3;
-    //mp3 = NULL;
-  }
-  if (buff) {
-    //Serial.println("Close buff");
-    //buff->close();
-    //delete buff;
-    //buff = NULL;
-  }
-  if (file) {
-    //Serial.println("Close file");
-    //file->close();
-    //delete file;
-    //file = NULL;
-  }
   Serial.println("Start new");
   file = new AudioFileSourceHTTPStream(musicUrl);
   buff = new AudioFileSourceBuffer(file, preallocateBuffer, preallocateBufferSize);
@@ -238,19 +274,23 @@ void loop() {
     next();
     buttonClicked = 0;
   } else if (buttonClicked == 2) {
-    Serial.println("Pause/Play");
     if (playing) {
+      Serial.println("Pause");
       out->stop(); 
     } else {
+      Serial.println("Play");
       out->begin();
     }
     playing = !playing;
+    buttonClicked = 0;
+  }else if (buttonClicked == 3) {
+    like();
     buttonClicked = 0;
   }else {
     if (mp3->isRunning()) {
       if (!mp3->loop()) {
         mp3->stop();
-        getMusic() && getMusicUrl() && playMusic();
+        next();
       } 
     } else {
       Serial.printf("MP3 done\n");
